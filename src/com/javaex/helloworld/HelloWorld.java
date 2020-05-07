326b45244588bc429d9df67b2459d4dd954620d8
'go-dw' <'rhehddnjs11@gmail.com'>
2020-05-07 10:27:34 +0900

    empty3331 1st commit

---
+++ src/com/javaex/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ 작성자:김주혜
+ 작성일:2020-05-01
+ 설 명:Hello World 출력하기
+ */
+
+package com.javaex.helloworld;
+
+public class HelloWorld {
+	
+	public static void main(String[] args){
+		
+		//Hello World 를 출력합니다
+        System.out.println("Hello World!");
+    }
+
+}
